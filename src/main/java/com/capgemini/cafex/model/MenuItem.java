2c0c4c6123ca84cb2223e54b2f5f604719240e4c
James Devaney <james.devaney@capgemini.com>
2017-06-02 15:03:05 +0100

    Simplify enum

--- src/main/java/com/capgemini/cafex/model/MenuItem.java
+++ src/main/java/com/capgemini/cafex/model/MenuItem.java
@@ -4,25 +4,19 @@ import java.math.BigDecimal;
 
 public enum MenuItem {
 
-	Cola("Cola", new BigDecimal("0.50"), false, false),
-	Coffee("Coffee", new BigDecimal("1.00"), true, false),
-	CheeseSandwich("Cheese Sandwich", new BigDecimal("2.00"), false, true),
-	SteakSandwich("Steak Sandwich", new BigDecimal("4.50"), true, true);
+	Cola(new BigDecimal("0.50"), false, false),
+	Coffee(new BigDecimal("1.00"), true, false),
+	CheeseSandwich(new BigDecimal("2.00"), false, true),
+	SteakSandwich(new BigDecimal("4.50"), true, true);
 
-	private final String name;
 	private final BigDecimal price;
 	private final boolean isHot;
 	private final boolean isFood;
 
-	MenuItem(String name, BigDecimal price, boolean isHot, boolean isFood) {
-		this.name = name;
+	MenuItem(BigDecimal price, boolean isHot, boolean isFood) {
 		this.price = price;
 		this.isHot = isHot;
 		this.isFood = isFood;
-	}
-
-	public String getName() {
-		return name;
 	}
 
 	public BigDecimal getPrice() {
